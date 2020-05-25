6bdccbf4ff2e2d257d28358cc281ce35dc0b0524
Chen <tuluwa@gmail.com>
2020-05-25 12:08:04 +0900

    changing development environment to MacOS

--- gym_showdown/envs/showdown_env.py
+++ gym_showdown/envs/showdown_env.py
@@ -1,23 +1,28 @@
 #main body of the showdownEnv class
 
 import gym
+import asyncio
 
-class ShowdownEnv(gym.env):
+class ShowdownEnv(gym.Env):
+
+
     metadata = {'render.modes':['human']}
     reward_range = [-1,1]
     action_space = [0,1,2,3,4,5,6,7,8,9,10] 
     #4 moves, 6 switches, 1 forfeit, 11 possible actions in total
     observation_space = None
 
-    def __init__(self):
-        #initialize
+    async def __init__(self):
+        #login and start battle
+        #preferrably using the ladder
+        from helpers.mywebsocket import showdownWebsocket as ws
         pass
 
-    def _step(self, action):
+    async def _step(self, action):
         #take action
         pass
 
-    def _reset(self):
+    async def _reset(self):
         #end epoch and start a new one
         pass
 
